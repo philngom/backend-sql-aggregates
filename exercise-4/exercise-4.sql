bb4da9e77f7af547decf762389542c2f0e346ef0
Philippe Ngom <philngom@gmail.com>
2022-03-23 15:57:00 -0700

    completed exercises 4-6

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -1,2 +1,18 @@
 -- the average film length by category
 -- include the category name and avg length
+
+SELECT
+name,
+AVG(film.length)
+FROM
+category
+LEFT JOIN
+film_category
+ON
+category.category_id = film_category.category_id
+LEFT JOIN
+film
+ON
+film.film_id = film_category.film_id
+GROUP BY
+category.name
--- exercise-6/exercise-6.sql
+++ exercise-6/exercise-6.sql
@@ -1,3 +1,24 @@
 -- customer ranking by total amount spent on *rentals*
 -- include email and rank
 -- use RANK ORDER BY the SUM of the amount in descending order
+
+SELECT
+customer.email,
+RANK () OVER (
+  ORDER BY
+  SUM(amount) DESC
+)
+FROM
+customer
+LEFT JOIN
+rental
+ON
+rental.customer_id = customer.customer_id
+LEFT JOIN
+payment
+ON
+payment.rental_id = rental.rental_id
+GROUP BY
+customer.email
+-- ORDER BY
+-- sum DESC
